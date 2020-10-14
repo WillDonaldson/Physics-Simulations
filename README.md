5b6ed7e652cb25067ad98bb8e97398981e036d26
Will Donaldson <WillDonaldson@users.noreply.github.com>
2020-10-13 19:15:37 -0700

    Added phase space plot

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 Fun projects, mostly for personal practice
 
 Documentation
+
 pendulum-small-angle.py
 A simulation of the classic mechanics problem. Solved using the small angle approximation. 
--- pendulum-small-angle.py
+++ pendulum-small-angle.py
@@ -1,23 +1,41 @@
-from numpy import cos, sin, pi, sqrt
+from numpy import cos, sin, pi
+import numpy as np
 from time import time
 import matplotlib.pyplot as plt 
 import matplotlib.animation as animation
 
 fig = plt.figure()
-ax1 = fig.add_subplot(111, aspect='equal')
-ax1.set_xlim(-1, 1)
-ax1.set_ylim(-1.5, 0.5)
-ax1.set_title("Pendulum Animation")
+ax1 = fig.add_subplot(121, aspect='equal', xlim = (-1, 1), ylim = (-1.5, 0.5), title = "Pendulum Animation")
+ax2 = fig.add_subplot(122, xlim = (-2*pi, 2*pi), ylim = (-10, 10), title = "Phase Space Plot")
+ax2.set_xlabel(r"$\Theta$[rad]")
+ax2.set_ylabel(r"$\dot{\Theta}$[rad/s]")
+
 ax1.grid()
+ax2.grid()
+
 line, = ax1.plot([], [], 'o-', lw=1)
 
 theta_0 = pi/8      #the method used in this program is only valid for theta_0 << 1 [rad]
 g = 9.81
 L = 1.0
-omega = sqrt(g/L)
+m = 1.0
+omega = np.sqrt(g/L)
 origin = (0,0)
 dt = 0.05
 t_elapsed = 0.0
+
+def Hamiltonian(q, p):
+    #Calculates the Hamiltonian of a simple pendulum
+    H = p**2 / (2*m*L**2) + m*g*L*(1-cos(q))
+    return H
+
+#Create points for plotting the phase space
+x = np.arange(-2*pi, 2*pi, 0.05)
+y = np.arange(-10, 10, 0.05)
+Theta, Theta_dot = np.meshgrid(x, y)            # poor notation, needs renaming. This variable is different to the theta in calculate_position()
+q = Theta
+p = m * L**2 * Theta_dot
+cs = ax2.contour(Theta, Theta_dot, Hamiltonian(q,p))
 
 def calculate_position(t):
     theta = theta_0 * cos(omega * t)            # valid for small angles
